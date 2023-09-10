782b8290909f1b9a1bd8b299bbbeb73111878360
Sam Bisaria <80646395+SamBisaria@users.noreply.github.com>
2023-09-10 15:32:51 -0400

    Time: 15 ms (63.05%), Space: 13.6 MB (38.73%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -8,16 +8,10 @@ class Solution(object):
         index = -1
         c = 0
         for a in range(len(s)):
-            print t.find(s[a], index+1)
-            print index
             if t.find(s[a], index+1) != -1:
-                #t.replace(s[a],'')
                 if index < 0:
                     index = 0
                 index = t.find(s[a], index+1)
                 c += 1
-                print "c:" + str(c)
-        if len(s) == c:
-            return True
-        return False
+        return len(s) == c
         
