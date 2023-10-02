35c6686847a09eece994e702c25cb55417263f45
Sam Bisaria <80646395+SamBisaria@users.noreply.github.com>
2023-10-02 15:25:14 -0400

    Time: 31 ms (7.29%), Space: 13.1 MB (97.16%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        numbers = []
+        occurences = []
+        for element in arr:
+            if element not in numbers:
+                numbers.append(element)
+                occurences.append(arr.count(element))
+        if len(set(occurences)) == len(occurences):
+            return True
+        return False
+        
+        
