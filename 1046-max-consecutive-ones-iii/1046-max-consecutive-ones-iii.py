b1969f8d9515b8793c68318d850ea24464422298
Sam Bisaria <80646395+SamBisaria@users.noreply.github.com>
2023-10-02 14:08:19 -0400

    Time: 444 ms (58.12%), Space: 14.2 MB (5.62%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def longestOnes(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        counter = 0
+        kcounter = k
+        maximum = 0
+        pointer = -1
+        zeroes = []
+        if k >= len(nums):
+            pass
+            #return len(nums)
+        while counter < len(nums):
+            if nums[counter] == 0:
+                if kcounter > 0:
+                    kcounter -= 1
+                    zeroes.append(counter)
+                else:
+                    if k == 0:
+                        pointer = counter
+                    else:
+                        pointer = zeroes[len(zeroes) - (k)]
+                        zeroes.append(counter)
+            maximum = max(counter - pointer, maximum)
+            counter += 1
+        return maximum
+                
+
+
+        
